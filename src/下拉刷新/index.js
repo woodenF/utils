d6519613808af3a290969b985cdce3e981795a43
woodenF <916885119@qq.com>
2020-05-22 11:04:41 +0800

    修复下拉刷新回拉问题

--- src/下拉刷新/index.js
+++ src/下拉刷新/index.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.refresh-container');
 const refresh = document.querySelector('.refresh');
 
 container.addEventListener('scroll', (e) => {
@@ -7,19 +7,23 @@ container.addEventListener('scroll', (e) => {
   console.log('内容高度', container.offsetHeight);
 });
 
-let startMoveY = 0;
+let startMoveY = null;
 let endMoveY = 0;
 let isRefresh = false;
 
 container.addEventListener('touchstart', (e) => {
-  if (getScrollTop()) {
+  isRefresh = true;
+  if (getScrollTop(document.querySelector('.content'))) {
     return;
   }
-  isRefresh = true;
   startMoveY = e.touches[0].clientY;
 });
 
 container.addEventListener('touchmove', (e) => {
+  if (getScrollTop(document.querySelector('.content'))) {
+    startMoveY = startMoveY || e.touches[0].clientY;
+    return;
+  }
   if(!isRefresh) {
     return;
   }
@@ -36,7 +40,7 @@ container.addEventListener('touchmove', (e) => {
   container.style = `
     top:${offsetY}px;
   `;
-  if(offsetY > -30) {
+  if(offsetY > -20) {
     refresh.innerHTML= '松开刷新';
   } else {
     refresh.innerHTML= '下拉刷新';
@@ -46,15 +50,21 @@ container.addEventListener('touchend', (e) => {
   isRefresh = false;
   const distance = endMoveY - startMoveY;
   const offsetY = distance - 100 > 0 ? 0 : distance - 100 < -100 ? -100 : distance - 100;
-  if (offsetY > -50) {
-    console.log('===');
+  if (offsetY > -20) {
+    setTimeout(() => {
+      container.style = `
+        top: ${-100}px;
+        transition: 1s;
+      `;
+    }, 3000);
+  } else {
+    container.style = `
+      top: ${-100}px;
+      transition: 1s;
+    `;
   }
-  container.style = `
-    top: ${-100}px;
-    transition: 1s;
-  `;
 
 });
-const getScrollTop = function() {
-  return container.scrollTop;
+const getScrollTop = function(el) {
+  return el.scrollTop;
 };
